1fbf144ec2175527e7a01ffea8b61f5766270dc7
insane-dev <delagics@gmail.com>
2016-05-10 00:53:08 +0300

    Updated UrlManager configuration, now used shorter language codes

--- app/base/config/main.php
+++ app/base/config/main.php
@@ -10,12 +10,8 @@ return [
             'class' => 'base\components\UrlManager',
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            // List all supported languages here
-            // Make sure, you include your app's default language.
-            'languages' => ['uk' => 'uk-UA', 'en' => 'en-US', 'ru' => 'ru-RU'],
-            'enableDefaultLanguageUrlCode' => true, // show default language in URL
-            'enableLanguagePersistence' => true, // remember selected language
-            'enableLanguageDetection' => true, // language detection
+            'languages' => ['uk', 'ru', 'en'],
+            'enableDefaultLanguageUrlCode' => true,
             'ignoreLanguageUrlPatterns' => [],
             'rules'=>[],
         ],
