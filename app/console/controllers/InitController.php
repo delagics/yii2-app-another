6b070c9f784b46bfc7d41c1b017f2f3acba0aa8b
insane-dev <delagics@gmail.com>
2016-05-15 20:26:46 +0300

    Improved InitController and added new action `init/env` to it.

--- app/console/controllers/InitController.php
+++ app/console/controllers/InitController.php
@@ -2,10 +2,14 @@
 
 namespace console\controllers;
 
+use console\helpers\Initializer;
 use Yii;
-use yii\console\Controller;
+use Exception;
+use yii\di\Instance;
+use yii\db\Connection;
 use yii\helpers\ArrayHelper;
 use console\helpers\Console;
+use console\components\Controller;
 
 /**
  * Class InitController
@@ -22,9 +26,53 @@ class InitController extends Controller
     public $defaultAction = 'up';
 
     /**
-     * @var \yii\db\Connection application db component.
+     * @var Connection|array|string the DB connection object or the application component ID of the DB connection to use
+     * when applying migrations. Starting from version 2.0.3, this can also be a configuration array
+     * for creating the object.
      */
-    private $_db;
+    public $db = 'db';
+
+    /**
+     * This method is invoked right before an action is to be executed (after all possible filters.)
+     * It checks the existence of the [[migrationPath]].
+     * @param \yii\base\Action $action the action to be executed.
+     * @return boolean whether the action should continue to be executed.
+     */
+    public function beforeAction($action)
+    {
+        if (parent::beforeAction($action)) {
+            $this->db = Instance::ensure($this->db, Connection::className());
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Set environment depending variables in the .env file.
+     *
+     * @return int the status of the action execution. 0 means normal, other values mean abnormal.
+     * @uses \console\helpers\Initializer::setEnv()
+     */
+    public function actionEnv()
+    {
+        $str = Console::ansiFormat('Which environment to use? (DEV|PROD)', [Console::FG_YELLOW]);
+        $answer = $this->prompt($str, ['default' => 'PROD']);
+
+        if (!strncasecmp($answer, 'd', 1)) {
+            $this->stdout('Setting DEV environment...', Console::FG_GREEN);
+            Initializer::setEnv('dev');
+        } elseif (!strncasecmp($answer, 'p', 1)) {
+            $this->stdout('Setting PROD environment...', Console::FG_GREEN);
+            Initializer::setEnv('prod');
+        } else {
+            $this->stderr('Environment you choose does not exist', Console::FG_RED);
+
+            return Controller::EXIT_CODE_ERROR;
+        }
+
+        return Controller::EXIT_CODE_NORMAL;
+    }
 
     /**
      * Prepare application by running depending modules migrations and other stuff.
@@ -34,17 +82,24 @@ class InitController extends Controller
      */
     public function actionUp()
     {
+        Yii::$app->runAction('init/env');
+
         /***************************************************************************************************************
          * Getting input data from console user.
          **************************************************************************************************************/
 
-        $email = $this->prompt('Enter global admin user email:', [
-            'default' => ArrayHelper::getValue(Yii::$app->params, 'adminEmail', 'admin@example.com'),
+        $emailStr = Console::ansiFormat('Enter admin user email:', [Console::FG_YELLOW]);
+        $email = $this->prompt($emailStr, [
+            'default' => env('APP_ADMIN_EMAIL', 'admin@example.com'),
         ]);
-        $username = $this->prompt('Enter global admin user name:', [
-            'default' => ArrayHelper::getValue(Yii::$app->getModule('user')->admins, 0, 'admin'),
+
+        $usernameStr = Console::ansiFormat('Enter admin user name:', [Console::FG_YELLOW]);
+        $username = $this->prompt($usernameStr, [
+            'default' => ArrayHelper::getValue(explode(',', env('APP_ADMINS', 'admin')), 0),
         ]);
-        $password = $this->prompt('Enter global admin user password:', ['default' => '123456']);
+
+        $passwordStr = Console::ansiFormat('Enter global admin user password:', [Console::FG_YELLOW]);
+        $password = $this->prompt($passwordStr, ['default' => '123456']);
 
         $this->askForInteraction();
 
@@ -54,12 +109,19 @@ class InitController extends Controller
 
         $migrations = explode(',', env('APP_MIGRATION_LOOKUP', '@console/migrations'));
 
-        foreach ($migrations as $migrationPath) {
-            Yii::$app->runAction('migrate/up', [
-                'migrationPath' => $migrationPath,
-                'interactive' => $this->interactive,
-            ]);
+        try {
+            foreach ($migrations as $migrationPath) {
+                Yii::$app->runAction('migrate/up', [
+                    'migrationPath' => $migrationPath,
+                    'interactive' => $this->interactive,
+                ]);
+            }
+        } catch (Exception $e) {
+            $this->stdout("Exception: {$e->getMessage()}\n", Console::FG_RED);
+            $this->stdout("({$e->getFile()}: {$e->getLine()})\n", Console::FG_RED);
+            return Controller::EXIT_CODE_ERROR;
         }
+
 
         /***************************************************************************************************************
          * Creating and confirming account of the first user.
@@ -133,7 +195,7 @@ class InitController extends Controller
      */
     public function actionDestroyDb()
     {
-        $str = Console::ansiFormat('Do you really want to destroy application database? (yes|no)', Console::FG_RED);
+        $str = Console::ansiFormat('Do you really want to destroy application database? (yes|no)', [Console::FG_YELLOW]);
         $destroy = $this->prompt($str, ['default' => 'no']);
         $destroy = $destroy == 'y' || $destroy == 'yes' ? true : false;
 
@@ -211,7 +273,7 @@ class InitController extends Controller
      */
     public function askForInteraction()
     {
-        $str = Console::ansiFormat('Do you want to interact with this script? (yes|no)', Console::FG_YELLOW);
+        $str = Console::ansiFormat('Do you want to interact with this script? (yes|no)', [Console::FG_YELLOW]);
         $isInteractive = $this->prompt($str, ['default' => 'no']);
         $this->interactive = $isInteractive == 'no' || $isInteractive == 'n' ? false : true;
     }
@@ -225,19 +287,5 @@ class InitController extends Controller
     public function countMigrations($alias)
     {
         return count(glob(Yii::getAlias("$alias/m*.php")));
-    }
-
-    /**
-     * Get current Connection.
-     *
-     * @return \yii\db\Connection
-     */
-    public function getDb()
-    {
-        if ($this->_db === null) {
-            $this->_db = Yii::$app->getDb();
-        }
-
-        return $this->_db;
     }
 }
