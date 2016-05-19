ba56d16a7ad14692cb5179f9f5241b454396bb58
delagics <delagics@gmail.com>
2016-05-19 15:41:11 +0300

    Added more DB env variables

--- app/base/config/main.php
+++ app/base/config/main.php
@@ -8,7 +8,12 @@ return [
             'dsn' => env('DB_DSN', 'mysql:host=localhost;port=3306;dbname=app_db'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
+            'tablePrefix' => env('DB_TABLE_PREFIX', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
+            'enableSchemaCache' => env('DB_SCHEMA_CACHE', false),
+            'schemaCacheDuration' => env('DB_SCHEMA_CACHE_DURATION', 3600),
+            'enableQueryCache' => env('DB_QUERY_CACHE', true),
+            'queryCacheDuration' => env('DB_QUERY_CACHE_DURATION', 3600),
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
